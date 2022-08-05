4b7f1f5fced69ffc3ef3a04dbb1e96ce04140c85
menezesphill <phillipe.o.m@hotmail.com>
2022-08-05 16:10:48 -0300

    feat: Fetching Peer Id from cluster to main page, ellipse needs styling

--- src/pages/Main/PeerID/index.tsx
+++ src/pages/Main/PeerID/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import useAppState, { Settings } from "./../../../state/index";
+import useUser, { UserState } from "./../../../state/user";
+
+
 
 const PeerID: React.FC = () => {
+
+  const settings = useAppState().state.settings as Settings;
+  const myPeerId = useUser(settings);
+  
+  console.log(myPeerId);
   return (
     <div className={styles.container}>
       <div className={styles.data}>
         <span>Your Peer ID is:</span>
         <div className={styles.id}>
-          <span>dainsdoaisndaiidan2...</span>
+          <span>{myPeerId.state.myPeerId}</span>
+        </div>
+        <div className={styles.pointer}>
           <img src="/images/copyDark.svg" alt="Icon" />
         </div>
+        
       </div>
       <div className={styles.divisor} />
     </div>
